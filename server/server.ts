9c8cf0a60baf682b49585946a56d9e9faabcf5a5
rinel <rineliniguezsosa@gmail.com>
2023-06-18 19:59:51 -0600

    feat(server): definimos el middle app.use para definir el titulo de entreda de las apis y le pasamos el router que lea las rutas de las solicitudes http
    titulo principal de las api sera "api" titulo por definir posteriormente

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 const app = express()
 
 app.use(express.json())
+app.use('/api',router)
 
 app.listen(`${process.env.PORTSERVER}`,()=>{
     console.log(`Server runing in the port http://localhost:${process.env.PORTSERVER}`)
