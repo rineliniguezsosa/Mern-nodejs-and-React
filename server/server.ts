8b17d7c7d1f50ddf2093e232faead73c1eeff770
rinel <rineliniguezsosa@gmail.com>
2023-06-18 19:39:12 -0600

    feat(server): creando el servidor mediante app.listen
    creamos el servidor en donde esteremos escuchando un puerto y retornaremos en consola un mensaje para poder redireccionarnos hacia el server

--- server/server.ts
+++ server/server.ts
@@ -3,3 +3,7 @@ import 'dotenv/config'
 const app = express()
 
 app.use(express.json())
+
+app.listen(`${process.env.PORTSERVER}`,()=>{
+    console.log(`Server runing in the port http://localhost:${process.env.PORTSERVER}`)
+})
