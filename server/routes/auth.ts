05438524322d6c4552bfda1687cce6c465544001
rinel <rineliniguezsosa@gmail.com>
2023-06-20 15:18:42 -0600

    fix(server): estableciendo la funcion login en la ruta "auth/login"

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,8 +2,6 @@ import { Router } from 'express'
 import { registrarse,login } from '../controllers'
 export const router = Router()
 
-router.get('/auth/login',(req,res)=>{
-    return res.json('Hola mundo')
-})
+router.get('/auth/login',login)
 
 router.get('/auth/registrarse',registrarse)
