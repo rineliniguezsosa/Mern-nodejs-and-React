cd8b2860560d60f8830c3eb93a27af96cfdd35c6
rinel <rineliniguezsosa@gmail.com>
2023-06-20 14:22:31 -0600

    feat(client): importando Approuter en el archivo main.tsx
    En Approuter definiremos nuestras rutas principales

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Approuter } from './router/Approuter.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
