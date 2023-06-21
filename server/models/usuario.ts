e08fa7b2fe9d7a4c4cc3543c34cece4faf02c3f8
rinel <rineliniguezsosa@gmail.com>
2023-06-21 17:04:34 -0600

    feat(server): creando la variable usuario donde crearemos un nuevo modelado de datos

--- server/models/usuario.ts
+++ server/models/usuario.ts
@@ -1,1 +1,26 @@
 import { Schema } from 'mongoose'
+
+const usuario = new Schema({
+    nombre:{
+        type:String,
+    }, 
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    hashedpassword:{
+        type:String,
+    },
+    salt:String,
+    role:{
+        type:String,
+        default:'subscriptor'
+    },
+    resetpasswordlink:{
+        data:String,
+        default:''
+    }
+},{ timestamps:true })
