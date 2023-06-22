76cd1133df2caf2408ec7c543bef3c9b0da38457
rinel <rineliniguezsosa@gmail.com>
2023-06-21 21:19:43 -0600

    feat(server): llamando a la función mongoconnection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { router } from '../routes'
-import { mongoconnection } from '../connectiondb'import { mongoconnection } from '../connectiondb'
+import { mongoconnection } from '../connectiondb'
 import 'dotenv/config'
 const app = express()
 
 app.use(express.json())
 app.use('/api',router)
+
+mongoconnection()//db
 
 app.listen(`${process.env.PORTSERVER}`,()=>{
     console.log(`Server runing in the port http://localhost:${process.env.PORTSERVER}`)
