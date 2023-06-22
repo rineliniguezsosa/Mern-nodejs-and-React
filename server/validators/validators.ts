c4dc60a268fed8a3fe967d77bf136348bcb0bd55
rinel <rineliniguezsosa@gmail.com>
2023-06-22 11:21:09 -0600

    feat(server): estableciendo la referencia de la funcion validarerrores en el array uservalidationregister

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -5,4 +5,5 @@ export const uservalidationregister = [
     check('nombre').notEmpty().withMessage('Requerido'),
     check('email').isEmail().withMessage("Es requerido un formato valido de Email"),
     check('password').isLength({min:6}).withMessage("Debe contener al menos 6 caracteres"),
+    validarerrores
 ]
