3d659770268b9425ce7477c8ddb07233bd9617af
rinel <rineliniguezsosa@gmail.com>
2023-06-22 14:15:24 -0600

    feat(server): creamos el condicional if para verificar la variable existemail
    si la variable existemail es verdadera entonces establecemos un error siguido por el mensaje "El ${email} ya esta registrado"

--- server/helpers/dbvalidators.ts
+++ server/helpers/dbvalidators.ts
@@ -2,4 +2,8 @@ import { usuario } from "../models";
 
 export const  existemail = async(email = "")=>{
     const existemail = await usuario.findOne({email})
+
+    if(existemail){
+        throw new Error(`El ${email} ya esta registrado`)
+    }
 }
