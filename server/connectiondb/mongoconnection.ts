b6adbd068b98f13b0e19ec009211d4623bb43157
rinel <rineliniguezsosa@gmail.com>
2023-06-21 21:06:25 -0600

    feat(server): definiendo el await o espera de la conexion de mongoose.connect

--- server/connectiondb/mongoconnection.ts
+++ server/connectiondb/mongoconnection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const mongoconnection = async() =>{
     try {
-        
+        await mongoose.connect(`${process.env.MONGOCONNECTIONSTRING}`)
     } catch (error) {
         
     }
