c1e7b95b4d39854696f20684e4185a06ebe17000
rinel <rineliniguezsosa@gmail.com>
2023-06-22 11:15:15 -0600

    feat(server): establecemos el condicional if donde negaremos error.isEmpty() para verificar que existen errores de validacion
    devolveremos un status 400 para devolver el mensaje de error del campo

--- server/middleware/validarerrores.ts
+++ server/middleware/validarerrores.ts
@@ -2,4 +2,8 @@ import { validationResult } from 'express-validator'
 
 export const validarerrores = async(req,resp,next)=>{
     const error = validationResult(req)
+
+    if(!error.isEmpty()){
+        return resp.status(400).json(error)
+    }
 }
