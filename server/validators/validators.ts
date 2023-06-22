e56e9fb7cf5c87b64a082b45e7659987ba888630
rinel <rineliniguezsosa@gmail.com>
2023-06-22 10:59:35 -0600

    feat(server): estableciendo la validacion del campo email
    el email sera un formato valido de email y en caso de no establecer un formato de email mostraremos el mansaje de "Es requerido un formato valido de Email" al usuario

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -2,4 +2,5 @@ import { check } from 'express-validator'
 
 export const uservalidationregister = [
     check('nombre').notEmpty().withMessage('Requerido'),
+    check('email').isEmail().withMessage("Es requerido un formato valido de Email"),
 ]
