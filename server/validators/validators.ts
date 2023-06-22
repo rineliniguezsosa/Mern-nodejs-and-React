c2d052894d9a186e473b38fcf291f48982c10e31
rinel <rineliniguezsosa@gmail.com>
2023-06-22 11:01:51 -0600

    feat(server): estableciendo la validacion del campo password
    el password debe de contener una longitud de 6 caracteres en caso de no cumplir con esta regal mostraremos el mensaje de "Debe contener al menos 6 caracteres"

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -3,4 +3,5 @@ import { check } from 'express-validator'
 export const uservalidationregister = [
     check('nombre').notEmpty().withMessage('Requerido'),
     check('email').isEmail().withMessage("Es requerido un formato valido de Email"),
+    check('password').isLength({min:6}).withMessage("Debe contener al menos 6 caracteres"),
 ]
