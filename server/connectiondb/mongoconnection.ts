e88c699ca54183e82f2a463540b7874f6d1caa3d
rinel <rineliniguezsosa@gmail.com>
2023-06-21 21:07:55 -0600

    feat(server): definiendo en consola el mensaje "Conexión exitosa a la base de datos"

--- server/connectiondb/mongoconnection.ts
+++ server/connectiondb/mongoconnection.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 export const mongoconnection = async() =>{
     try {
         await mongoose.connect(`${process.env.MONGOCONNECTIONSTRING}`)
+
+        console.log("Conexión exitosa a la base de datos.")
     } catch (error) {
         
     }
