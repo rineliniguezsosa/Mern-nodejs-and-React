1a6a4b827699e01691259ed074d5cb1aea3536d9
rinel <rineliniguezsosa@gmail.com>
2023-06-23 14:58:16 -0600

    feat(server): estableciendo las propiedades para el tipado de la interface Usuario
    tipando las propiedades nombre,email, password, hashedpassword, salt, resetpasswordlink como string y estableciendo el role como el tipo subscriptor

--- server/Types/Interfaces.ts
+++ server/Types/Interfaces.ts
@@ -1,5 +1,11 @@
 import { Document } from 'mongoose'
 
 export interface Usuario {
-
+    nombre:string;
+    email:string;
+    password:string;
+    hashedpassword:string;
+    salt:string;
+    role:'subscriptor';
+    resetpasswordlink:string;
 }
