dcb2e3ec66c677e47d061bd1a1a4784700bc0453
rinel <rineliniguezsosa@gmail.com>
2023-06-22 20:08:09 -0600

    feat(server): estableciendo el middleware userloginvalidator para las validaciones de campos

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -3,7 +3,7 @@ import { registrarse,login,activatucuenta } from '../controllers'
 import { uservalidationregister,userloginvalidator } from '../validators' 
 export const router = Router()
 
-router.get('/auth/login',login)
+router.get('/auth/login',userloginvalidator,login)
 
 router.post('/auth/registrarse',uservalidationregister,registrarse)
 
