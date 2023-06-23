ecb3bd0a8a36df12af05f95f27e3390961a8d319
rinel <rineliniguezsosa@gmail.com>
2023-06-22 20:32:55 -0600

    feat(server): estableciiendo las validaciones de los campos email y password
    verificando que el campo email sea del formato email, password debe contener una longitud de 6 caracteres

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -11,5 +11,7 @@ export const uservalidationregister = [
 ]
 
 export const userloginvalidator = [
-
+    check('email').isEmail().withMessage("Es requerido un formato valido de Email"),
+    check('password').isLength({min:6}).withMessage("Debe contener al menos 6 caracteres"),
+    validarerrores
 ]
