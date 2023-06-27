3206ecbf2315e1d8ee69b54ca424797ddfacfe34
rinel <rineliniguezsosa@gmail.com>
2023-06-27 15:19:32 -0600

    feat(client): estableciendo el titulo y el children en el contenido tsx de AuthLayout.tsx

--- client/src/auth/Layout/AuthLayout.tsx
+++ client/src/auth/Layout/AuthLayout.tsx
@@ -7,9 +7,10 @@ export const AuthLayout = ({children,titulo}:AuthLayoutprops) => {
     <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
       
       <Grid item xs={3} sx={{border:'1px red solid',padding:3,borderRadius:2,width:{sm:450}}}>
-          <Typography variant="h5" mb={1}>Registrarse</Typography>
+          <Typography variant="h5" mb={1}>{titulo}</Typography>
       </Grid>
-
+      
+      {children}
     </Grid>
   )
 }
