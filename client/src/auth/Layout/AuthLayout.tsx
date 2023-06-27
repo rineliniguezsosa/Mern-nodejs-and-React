1fea8253369742686b4c74794f1d16a99878e2e4
rinel <rineliniguezsosa@gmail.com>
2023-06-27 17:01:51 -0600

    fix(client): eliminando la propiedad border

--- client/src/auth/Layout/AuthLayout.tsx
+++ client/src/auth/Layout/AuthLayout.tsx
@@ -6,7 +6,7 @@ export const AuthLayout = ({children,titulo}:AuthLayoutprops) => {
   return (
     <Grid container spacing={0} mt={5} direction="column" alignItems='center' justifyContent='center'>
       
-      <Grid item xs={3} sx={{border:'1px red solid',borderRadius:2,width:{sm:450}}}>
+      <Grid item xs={3} sx={{borderRadius:2,width:{sm:450}}}>
           <Typography variant="h5" mb={1}>{titulo}</Typography>
       </Grid>
       
