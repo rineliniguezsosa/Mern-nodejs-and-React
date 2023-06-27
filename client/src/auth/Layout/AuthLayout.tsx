df0ae967424295539baba471ab6df88bb2659011
rinel <rineliniguezsosa@gmail.com>
2023-06-27 15:01:34 -0600

    feat(client): pegando el contenido de Registrarse.tsx en AuthLayout.tsx

--- client/src/auth/Layout/AuthLayout.tsx
+++ client/src/auth/Layout/AuthLayout.tsx
@@ -2,6 +2,12 @@
 
 export const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
+      
+      <Grid item xs={3} sx={{border:'1px red solid',padding:3,borderRadius:2,width:{sm:450}}}>
+          <Typography variant="h5" mb={1}>Registrarse</Typography>
+      </Grid>
+
+    </Grid>
   )
 }
