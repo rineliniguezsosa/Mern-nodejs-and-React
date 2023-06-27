1cfad643ce4c1090409313b6214be5dfa23d4de0
rinel <rineliniguezsosa@gmail.com>
2023-06-27 16:51:59 -0600

    fix(client): eliminando las propiedades border,padding y añadiendo margin-top de 5 al componente Grid

--- client/src/auth/Layout/AuthLayout.tsx
+++ client/src/auth/Layout/AuthLayout.tsx
@@ -4,9 +4,9 @@ import { AuthLayoutprops } from '../../types/Datatypes';
 
 export const AuthLayout = ({children,titulo}:AuthLayoutprops) => {
   return (
-    <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
+    <Grid container spacing={0} mt={5} direction="column" alignItems='center' justifyContent='center'>
       
-      <Grid item xs={3} sx={{border:'1px red solid',padding:3,borderRadius:2,width:{sm:450}}}>
+      <Grid item xs={3} sx={{border:'1px red solid',borderRadius:2,width:{sm:450}}}>
           <Typography variant="h5" mb={1}>{titulo}</Typography>
       </Grid>
       
