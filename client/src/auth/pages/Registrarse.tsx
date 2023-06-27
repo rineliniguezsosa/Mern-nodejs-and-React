f723e32df41e6f4ea41fbc03911ee603efe1fe50
rinel <rineliniguezsosa@gmail.com>
2023-06-27 15:32:51 -0600

    fix(client): eliminando el contenido de Registrase y estableciendo el componente AuthLayout con la propiedad titulo="Registrarse"

--- client/src/auth/pages/Registrarse.tsx
+++ client/src/auth/pages/Registrarse.tsx
@@ -3,12 +3,10 @@ import { AuthLayout } from '../Layout'
 
 export const Registrarse = () => {
   return (
-    <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
+    <AuthLayout titulo="Registrase">
       
-      <Grid item xs={3} sx={{border:'1px red solid',padding:3,borderRadius:2,width:{sm:450}}}>
-          <Typography variant="h5" mb={1}>Registrarse</Typography>
-      </Grid>
+      
 
-    </Grid>
+    </AuthLayout>
   )
 }
