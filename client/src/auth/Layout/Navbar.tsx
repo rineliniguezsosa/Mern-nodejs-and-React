bc4770832a9ecb7ea0d5cc5aacd8330d38e18bb1
rinel <rineliniguezsosa@gmail.com>
2023-06-26 20:48:32 -0600

    feat(client): estableciendo el componente Toolbar

--- client/src/auth/Layout/Navbar.tsx
+++ client/src/auth/Layout/Navbar.tsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <AppBar position='static'>
-
+        <Toolbar>
+          
+        </Toolbar>
     </AppBar>
   )
 }
