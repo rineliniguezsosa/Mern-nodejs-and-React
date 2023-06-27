9e3ad3692b09f68b747bd56b5b2ae636f145d3f7
rinel <rineliniguezsosa@gmail.com>
2023-06-27 17:17:56 -0600

    feat(client): estableciendo el componente Grid y TextField para el elemento email

--- client/src/auth/pages/Registrarse.tsx
+++ client/src/auth/pages/Registrarse.tsx
@@ -7,9 +7,16 @@ export const Registrarse = () => {
       
       <form>
         <Grid container>
-            <Grid item xs={12} sx={{mt:2,width:{sm:450}}}>
+            <Grid item xs={12} sx={{mt:2}}>
                 <TextField
                 name="nombre" 
+                fullWidth
+                />
+            </Grid>
+
+            <Grid item xs={12} sx={{mt:2}}>
+                <TextField
+                name="email" 
                 fullWidth
                 />
             </Grid>
