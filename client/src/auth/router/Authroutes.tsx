506af67606050056b5feba2498cc6b6f47f18db0
rinel <rineliniguezsosa@gmail.com>
2023-06-26 20:37:24 -0600

    feat(client): importando el componente Home en el archivo Authroutes.tsx

--- client/src/auth/router/Authroutes.tsx
+++ client/src/auth/router/Authroutes.tsx
@@ -1,9 +1,10 @@
 import { Routes,Route } from 'react-router-dom'
+import { Home } from '../pages/Home';
 
 export const Authroutes = () => {
   return (
     <Routes>
-      
+
     </Routes>
   )
 }
