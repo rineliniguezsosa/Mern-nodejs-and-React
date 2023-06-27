05bfd71c811cc8d0d3dc30629a0d56356b9e47f0
rinel <rineliniguezsosa@gmail.com>
2023-06-27 17:54:58 -0600

    feat(client): estableciendo la propiedad onChange a los componentes TextField nombre,email y password
    el evento onChange ejecuta por referencia la funcion handlechange

--- client/src/auth/pages/Registrarse.tsx
+++ client/src/auth/pages/Registrarse.tsx
@@ -11,21 +11,24 @@ export const Registrarse = () => {
         <Grid container sx={{width:{sm:450}}}>
             <Grid item xs={12} sx={{mt:2}}>
                 <TextField
-                name="nombre" 
+                name="nombre"
+                onChange={handlechange} 
                 fullWidth
                 />
             </Grid>
 
             <Grid item xs={12} sx={{mt:2}}>
                 <TextField
-                name="email" 
+                name="email"
+                onChange={handlechange}  
                 fullWidth
                 />
             </Grid>
 
             <Grid item xs={12} sx={{mt:2}}>
                 <TextField
-                name="password" 
+                name="password"
+                onChange={handlechange}  
                 fullWidth
                 />
             </Grid>
