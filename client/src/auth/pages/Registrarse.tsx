e33e20e89f0b6a694d8300343c398721729ba26d
rinel <rineliniguezsosa@gmail.com>
2023-06-27 14:55:54 -0600

    feat(client): estableciendo el component Typography
    titulo Registrarse variant="h5" y mb como el margin-bottom:1

--- client/src/auth/pages/Registrarse.tsx
+++ client/src/auth/pages/Registrarse.tsx
@@ -5,7 +5,7 @@ export const Registrarse = () => {
     <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
       
       <Grid item xs={3} sx={{border:'1px red solid',padding:3,borderRadius:2,width:{sm:450}}}>
-          Registrarse
+          <Typography variant="h5" mb={1}>Registrarse</Typography>
       </Grid>
 
     </Grid>
