9138c1ca9ee7ad79053b68b6774e5aebf571b22b
rinel <rineliniguezsosa@gmail.com>
2023-06-27 14:24:13 -0600

    feat(client): estableciendo el componente Grid como contenedor padre
    props container,spacing={0} para no establecer espacio entre los elementos hijos,direction="column",alignItems y justifyContent como center para establecer el elemento hijo centrado y la prop sx para añadir un border color rojo

--- client/src/auth/pages/Registrarse.tsx
+++ client/src/auth/pages/Registrarse.tsx
@@ -2,6 +2,8 @@ import { Grid } from '@mui/material'
 
 export const Registrarse = () => {
   return (
-    <div>Registrarse</div>
+    <Grid container spacing={0} direction="column" alignItems='center' justifyContent='center' sx={{border:'1px red solid'}}>
+      Registrarse
+    </Grid>
   )
 }
