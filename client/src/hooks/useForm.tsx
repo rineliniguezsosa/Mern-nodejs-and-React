0c49e0b03518d81fa6ba9384f6ca2632cbdc3241
rinel <rineliniguezsosa@gmail.com>
2023-06-27 18:23:57 -0600

    feat(client): creando la funcion handlechange donde tomaremos los valores del formulario

--- client/src/hooks/useForm.tsx
+++ client/src/hooks/useForm.tsx
@@ -5,6 +5,10 @@ import { RegisterFormTypes } from '../types/Datatypes';
 export const useForm = (initialForm = {}) =>{
     const [formState, sEtformState] = useState(initialForm)
 
+    const handlechange = () =>{
+        
+    }
+
     return {
 
     }
