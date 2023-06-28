fe0a6c598464e260d51c8d20d2229166451dbaa5
rinel <rineliniguezsosa@gmail.com>
2023-06-28 15:19:12 -0600

    feat(server): importando la dependencia cors en el archivo server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { router } from '../routes'
+import cors from 'cors'
 import { mongoconnection } from '../connectiondb'
 import 'dotenv/config'
 const app = express()
