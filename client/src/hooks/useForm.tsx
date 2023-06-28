46a82d109efe395cb3186a4833d1e88ea8bae068
rinel <rineliniguezsosa@gmail.com>
2023-06-27 20:57:37 -0600

    fix(client): estableciendo el type RegisterFormTypes
    -estableciendo el type RegisterFormTypes al argumento initialForm
    -estableciendo el type RegisterFormTypes al valor futuro del useState
    -impimiendo en consola el formState

--- client/src/hooks/useForm.tsx
+++ client/src/hooks/useForm.tsx
@@ -2,9 +2,9 @@ import { useState,ChangeEvent } from 'react';
 import { RegisterFormTypes } from '../types/Datatypes';
 
 
-export const useForm = (initialForm = {}) =>{
-    const [formState, setformState] = useState(initialForm)
-
+export const useForm = (initialForm:RegisterFormTypes) =>{
+    const [formState, setformState] = useState<RegisterFormTypes>(initialForm)
+    console.log(formState)
     const handlechange = (event:ChangeEvent<HTMLInputElement>) =>{
         const { name,value } = event.target
         setformState({
