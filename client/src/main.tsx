310497de91d7bb9e0a29006daee9b115ab1c3f26
rinel <rineliniguezsosa@gmail.com>
2023-06-27 21:53:15 -0600

    feat(client): importando dotenv en el archivo main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Approuter } from './router/Approuter.tsx'
+import dotenv from 'dotenv'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
