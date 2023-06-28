add1320e6f52ce85e3940718d9758ee429e5a221
rinel <rineliniguezsosa@gmail.com>
2023-06-27 18:58:46 -0600

    feat(client): estableciendo el objeto event como parametro en la función handlechange
    tipando el objeto event como changeEvent del elemento HTMLInputElement

--- client/src/hooks/useForm.tsx
+++ client/src/hooks/useForm.tsx
@@ -5,7 +5,7 @@ import { RegisterFormTypes } from '../types/Datatypes';
 export const useForm = (initialForm = {}) =>{
     const [formState, setformState] = useState(initialForm)
 
-    const handlechange = () =>{
+    const handlechange = (event:ChangeEvent<HTMLInputElement>) =>{
 
     }
 
