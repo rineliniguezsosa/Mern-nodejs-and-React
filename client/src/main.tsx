bb5e12ff82759d348f6fb12785a6da11ad14f644
rinel <rineliniguezsosa@gmail.com>
2023-06-27 21:55:21 -0600

    feat(client): definiendo la configuración dotenv.config() para cargar automaticamente las variables de entorno en el archivo main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Approuter } from './router/Approuter.tsx'
 import dotenv from 'dotenv'
 
+dotenv.config()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
