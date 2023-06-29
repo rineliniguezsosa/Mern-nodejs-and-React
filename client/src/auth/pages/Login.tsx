baf86eaf4cb764519d630b2d3d06062a4e87acd5
rinel <rineliniguezsosa@gmail.com>
2023-06-29 17:35:12 -0600

    feat(client): estableciendo la etiqueta html form en el archivo Login.tsx

--- client/src/auth/pages/Login.tsx
+++ client/src/auth/pages/Login.tsx
@@ -5,7 +5,9 @@ import { AuthLayout } from '../Layout'
 export const Login = () => {
   return (
     <AuthLayout titulo="Login">
-
+        <form>
+            
+        </form>
     </AuthLayout>
   )
 }
