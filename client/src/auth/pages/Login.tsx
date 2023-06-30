f60b66b7f6b44ea193d0c546e5e1e0df2eb48085
rinel <rineliniguezsosa@gmail.com>
2023-06-30 10:18:51 -0600

    feat(client): estableciendo el componente TextField
    -propiedad name="email" para el rastreo del input
    -propiedad onChange donde ejecutara por referencia la funcion handlechange
    -propiedad fullWidth para que el componente TextField ocupe el ancho de componente Grid
    -propiedad placeholder="Email"
    -propiedad value como el formState.email donde estableceremos todo el contenido de lo que el usuario escriba en el input text

--- client/src/auth/pages/Login.tsx
+++ client/src/auth/pages/Login.tsx
@@ -8,7 +8,16 @@ export const Login = () => {
     <AuthLayout titulo="Login">
         <form>
           <Grid container sx={{width:{sm:450}}}>
-            
+              <Grid item xs={12} sx={{mt:2}}>
+                    <TextField
+                    name="email"
+                    onChange={handlechange} 
+                    fullWidth
+                    type="text"
+                    placeholder="Email"
+                    value={formState.email}
+                    />
+                </Grid>
           </Grid>
         </form>
     </AuthLayout>
