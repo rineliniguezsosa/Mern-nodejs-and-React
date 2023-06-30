5a8f8126cf18d73b2f893cdee13639406533345a
rinel <rineliniguezsosa@gmail.com>
2023-06-30 10:09:01 -0600

    feat(client): estableciendo el componente Grid
    -propiedad container
    -propiedad sx para establecer estilos css donde definimos el width a 600px

--- client/src/auth/pages/Login.tsx
+++ client/src/auth/pages/Login.tsx
@@ -1,12 +1,15 @@
 import { Grid,TextField,Button } from '@mui/material'
 import { AuthLayout } from '../Layout'
+import { useForm } from '../../hooks/useForm';
 
 
 export const Login = () => {
   return (
     <AuthLayout titulo="Login">
         <form>
+          <Grid container sx={{width:{sm:450}}}>
             
+          </Grid>
         </form>
     </AuthLayout>
   )
