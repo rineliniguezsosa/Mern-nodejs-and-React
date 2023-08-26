f22675bf148ef4396600498dfe1ad1188100fad4
rinel <rineliniguezsosa@gmail.com>
2023-08-26 00:29:44 -0600

    fix(server): cambiando el mensaje de error del campo de entrada email
    - cambiando el mensaje por "*Es necesario un formato valido de Email"

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -4,7 +4,7 @@ import { existemail } from '../helpers'
 
 export const uservalidationregister = [
     check('nombre').notEmpty().withMessage('*El campo Nombre es requerido'),
-    check('email').isEmail().withMessage("Es requerido un formato valido de Email"),
+    check('email').isEmail().withMessage("*Es necesario un formato valido de Email"),
     check('email').custom(existemail), //valida si el email ya existe
     check('password').isLength({min:6}).withMessage("Debe contener al menos 6 caracteres"),
     validarerrores
