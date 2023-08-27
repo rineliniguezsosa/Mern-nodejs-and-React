682ce559dd9aa75386cc02f075ad1602f6b5ffc8
rinel <rineliniguezsosa@gmail.com>
2023-08-26 22:58:42 -0600

    feat(client): creando la ruta activate y pasando el parametro token
    - importando el componente Activate de la carpeta pages
    - la ruta activate devolvera el contenido del componente Activate

--- client/src/auth/router/Authroutes.tsx
+++ client/src/auth/router/Authroutes.tsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/Home';
 import { Navbar } from '../Layout/Navbar';
 import { Registrarse } from '../pages/Registrarse';
 import { Login } from '../pages/Login';
+import { Activate } from '../pages';
 
 
 export const Authroutes = () => {
@@ -15,6 +16,8 @@ export const Authroutes = () => {
         <Route path="/registrarse" element={<Registrarse/>}/>
 
         <Route path="/login" element={<Login/>}/>
+
+        <Route path="/activate:token" element={<Activate/>}/>
     </Routes>
     </>
   )
