8a20b46df6be8eaa2ae34b0fabc70025d82c1661
rinel <rineliniguezsosa@gmail.com>
2023-08-26 23:04:28 -0600

    fix(client). modificando la importación del componente Navbar para que la lectura de la importación sea más facil
    - importando los elementos de la carpeta Layout

--- client/src/auth/router/Authroutes.tsx
+++ client/src/auth/router/Authroutes.tsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from 'react-router-dom'
 import { Home } from '../pages/Home';
-import { Navbar } from '../Layout/Navbar';
+import { Navbar } from '../Layout';
 import { Activate,Login,Registrarse } from '../pages';
 
 
