f1e4d5c71f009a6c1909fb7573203159d64945b2
rinel <rineliniguezsosa@gmail.com>
2023-08-26 18:21:26 -0600

    feat(server): estableciendo la validación del campo email en las validaciones de uservalidationregister
    - si el campo esta vacio mostramos el mensaje de error "*El campo Email es requerido"

--- server/validators/validators.ts
+++ server/validators/validators.ts
@@ -4,6 +4,7 @@ import { existemail,Noexistemail } from '../helpers'
 
 export const uservalidationregister = [
     check('nombre').notEmpty().withMessage('*El campo Nombre es requerido'),
+    check('email').notEmpty().withMessage('*El campo Email es requerido'),
     check('email').isEmail().withMessage("*Es necesario un formato valido de Email"),
     check('email').custom(existemail), //valida si el email ya existe
     check('password').isLength({min:6}).withMessage("*El campo Password debe de contener al menos 6 caracteres"),
