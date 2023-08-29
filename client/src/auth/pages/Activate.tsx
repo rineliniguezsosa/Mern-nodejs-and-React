0cff891fa3a6154f7b5ef56fc6b3e9a1bec621b0
rinel <rineliniguezsosa@gmail.com>
2023-08-28 19:02:58 -0600

    feat: importando la dependencia de query-string y imprimiendo en consola el valor de location

--- client/src/auth/pages/Activate.tsx
+++ client/src/auth/pages/Activate.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
-
+import queryString from 'query-string';
 
 export const Activate = () => {
   const location = useLocation()
-  console.log(token)
+  console.log(location)
   return (
     <div>Activate</div>
   )
