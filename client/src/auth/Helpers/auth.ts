48cd7b230f29f25f636b0051d3a346cf418ee8cf
rinel <rineliniguezsosa@gmail.com>
2023-08-30 16:36:26 -0600

    feat: creando la variable req donde esperamos la petición http hacia nuestro servidor para activar la cuenta
    - pasamos el parametro del token al request.body de la solicitud http

--- client/src/auth/Helpers/auth.ts
+++ client/src/auth/Helpers/auth.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const activate = async(token = '') => {
     try {
-        
+       const req = await axios.post("http://localhost:5000/api/auth/registrarse",token)
     } catch (error) {
         
     }
