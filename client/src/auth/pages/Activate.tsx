37f61d0089f20a10f019c590c4759ba07f9962eb
rinel <rineliniguezsosa@gmail.com>
2023-08-30 16:07:35 -0600

    fix: eliminando la importación de axios en el archivo Activate.tsx

--- client/src/auth/pages/Activate.tsx
+++ client/src/auth/pages/Activate.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 import queryString from 'query-string';
-import axios from "axios";
+
 
 export const Activate = () => {
   const location = useLocation()
