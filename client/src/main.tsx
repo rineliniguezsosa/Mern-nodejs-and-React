6bbb451299efcd53989b484d6603eb2f57694987
rinel <rineliniguezsosa@gmail.com>
2023-08-30 19:14:18 -0600

    feat: importando el archivo index.css en el archivo main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Approuter } from './router/Approuter.tsx'
+import './index.css'
 // import dotenv from 'dotenv'
 
 // dotenv.config()
