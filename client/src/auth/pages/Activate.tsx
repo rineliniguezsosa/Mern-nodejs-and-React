08ad392f2503a89cb518b034e9dca02dae02a137
rinel <rineliniguezsosa@gmail.com>
2023-08-30 22:57:01 -0600

    fix: eliminando las propiedades css border-black y border-2 del contenedor Grid el cual almacena el botón

--- client/src/auth/pages/Activate.tsx
+++ client/src/auth/pages/Activate.tsx
@@ -33,7 +33,7 @@ export const Activate = () => {
             <Typography className="font-sans font-bold text-[15px] text-center">¡Bienvenido de vuelta rinel! Haz clic para activar tu cuenta ahora mismo.</Typography>
         </Grid>
 
-        <Grid item xs={12} className="border-black border-2 grid justify-items-center items-center">
+        <Grid item xs={12} className="grid justify-items-center items-center">
           <Button variant="outlined">Activa tu cuenta</Button>
         </Grid>
 
