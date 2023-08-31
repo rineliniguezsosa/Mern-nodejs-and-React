13bc09dd13e68e80273199a39e8091395512dce1
rinel <rineliniguezsosa@gmail.com>
2023-08-30 18:42:49 -0600

    feat: importando useState en el archivo Activate.tsx

--- client/src/auth/pages/Activate.tsx
+++ client/src/auth/pages/Activate.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 import { useLocation } from "react-router-dom"
 import queryString from 'query-string';
 import axios from "axios";
