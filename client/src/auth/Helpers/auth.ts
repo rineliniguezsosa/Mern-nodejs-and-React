151fb0d5f0e8f389a682747ff36aaaef88cdfdd5
rinel <rineliniguezsosa@gmail.com>
2023-09-01 19:15:58 -0600

    fix: eliminando todo del archivo auth.ts
    - estoy considerando que en este archivo establecer métodos reutilizables como por ejemplo la función closeSnackbar se utiliza en el archivo Activate.tsx imparcialmente y se usa en el hook useForm.ts, estare pensando que poder hacer con este archivo

--- client/src/auth/Helpers/auth.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const activate = async(token = '') => {
-    try {
-       const req = await axios.post("http://localhost:5000/api/auth/registrarse",token)
-    } catch (error) {
-        
-    }
-}
