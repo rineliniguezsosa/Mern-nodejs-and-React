85e434dd160b05cdf05c03be7d3b5b76177ce612
rinel <rineliniguezsosa@gmail.com>
2023-09-01 18:29:47 -0600

    feat: customizando los colores de tailwindcss
    establecimos el color de la aplicación para que pueda ser utilizada cuantas beses sea

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'colorapp':'#1976D2'
+      }
+    },
   },
   plugins: [],
 }
