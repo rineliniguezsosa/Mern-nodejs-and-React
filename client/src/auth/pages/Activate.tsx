76dbc92c92f6615e1d7aa30eaa7945d770e7b7c0
rinel <rineliniguezsosa@gmail.com>
2023-09-01 20:00:26 -0600

    fix: eliminando todo la logica del archivo Activate.tsx

--- client/src/auth/pages/Activate.tsx
+++ client/src/auth/pages/Activate.tsx
@@ -4,43 +4,10 @@ import { useActivateaccount } from '../../hooks'
 import MuiAlert from '@mui/material/Alert';
 
 export const Activate = () => {
-  const location = useLocation()
-  const [searchParams] = useSearchParams();
   //const [decodetoken, setdecodeToken] = useState<RegisterFormTypes | null>(null)
   //console.log(decodetoken)
-  
-  const [usuario, setUsuario] = useState<Usuario>({
-    nombre:'',
-    token:'',
-    show:false
-  })
-  console.log(usuario);
-  const { token,nombre } = usuario;
-
-  const activate = async() => {
-    try {
-       const req = await axios.post("http://localhost:5000/api/auth/activa-tu-cuenta",{token:token})
-       console.log(req.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
-  useEffect(() => {
-    const token = searchParams.get('token');
-    if(token){
-      try{
-        const tokendecode: RegisterFormTypes = jwtDecode(token as string); //decode payload
-        const { nombre } = tokendecode;
-        console.log(tokendecode)
-        setUsuario({...usuario,nombre:nombre,token:token,show:true})
-        //setdecodeToken(tokendecode);
-      }catch(error){
-        console.log(error)
-      }
-    } 
-  }, [location.search])
   const { nombre,activate } = useActivateaccount()
+
   return (
     <Grid container className="h-screen flex flex-col justify-center items-center">
 
