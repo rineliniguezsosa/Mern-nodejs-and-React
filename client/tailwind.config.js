29d504a98de2a4fe843fa8848eb92d4e3952e791
rinel <rineliniguezsosa@gmail.com>
2023-09-04 01:11:07 -0600

    fix: eliminando la medida 2xl de los breakpoints
    - estableciendo comentarios sobre las medidas establecidas
    - estableciendo la coma en la linea 13 corrigiendo el error

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,12 +10,11 @@ export default {
         'colorapp':'#1976D2'
       },
       screens: {
-        'xs':'300px'
-        'sm': '600px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+        'xs':'300px', // dispositivos mobiles más pequeño
+        'sm': '600px', //tablet
+        'md': '768px', //dispositivos medianos
+        'lg': '1024px', //laptop
+        'xl': '1280px' //desktop
       }
     },
   },
