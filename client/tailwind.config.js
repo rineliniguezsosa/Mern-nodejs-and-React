2c85fba6bf955de49317b2039d1ec0e928ae8233
rinel <rineliniguezsosa@gmail.com>
2023-09-04 01:05:16 -0600

    feat: customizando los breakpoints de tailwindcss de nuestro proyecto mern-stack
    - para pantallas pequeñas establecimos un media query minimo de 300px
    - para pantallas como tabletas establecimos un tamaño minimo de 600px
    - para pantallas de dispositivos medianos establecimos la medida como minimo de 768px
    - para pantallas como laptops definimos un tamaño minimo de 1024
    - para pantallas como escritorio definimos un tamaño minimo de 1280px

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,14 @@ export default {
     extend: {
       colors:{
         'colorapp':'#1976D2'
+      },
+      screens: {
+        'xs':'300px'
+        'sm': '600px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
